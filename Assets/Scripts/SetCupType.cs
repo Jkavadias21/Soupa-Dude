2a520a1116374db7fba9003a9c1954ec0d31adff
Jkavadias21 <johnnykavadias@gmail.com>
2023-01-26 23:38:20 +1100

    implemented no straw to hook straw animation overriding

---
+++ Assets/Scripts/SetCupType.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SetCupType : MonoBehaviour
+{
+    [SerializeField] private AnimatorOverrideController[] overrideControllers;
+    [SerializeField] private AnimatorOverrider overrider;
+
+    public void Set(int value)
+    {
+        overrider.SetAnimation(overrideControllers[value]);
+    }
+
+    public void Update()
+    {
+
+        if (SceneManager.GetActiveScene().name.Equals("Level 2"))
+        {
+            Set(0);
+        }
+        
+    }
+}
